0e940c4f7bb76bc37cab3093988ee7c6122daf9c
bellamy.dev <slyder.dev@gmail.com>
2026-03-06 23:29:20 +0100

    commit 6 mars

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import ProductForm from './ProductForm'
+import './App.css'
+
+// le composant principal
+function App() {
+  return (
+    <div>
+      <h1>Mon appli produits</h1>
+      <ProductForm />
+    </div>
+  )
+}
+
+export default App
